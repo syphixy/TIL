18ac9891a8546899a0840292ef3806d7cd631e1b
Artem Snisarenko <snisarenkoartem@gmail.com>
2023-05-22 21:44:08 -0400

    Initial Commit

---
+++ TIL/TILApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILApp.swift
+//  TIL
+//
+//  Created by Artem on 2023-05-22.
+//
+
+import SwiftUI
+
+@main
+struct TILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
